308e9d5aef97578f8924b900495754a2ae501662
audrey-audrey <audreyuu@gmail.com>
2021-02-05 01:27:56 +0000

    Set up about#index page

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  resources :about, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
