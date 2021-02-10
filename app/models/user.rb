583fd3c810435535c6c91956ebf1a814454721e5
audrey-audrey <audreyuu@gmail.com>
2021-02-10 05:23:19 +0000

    Implement authenticate_with_credentials for session

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,14 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password, length: { minimum: 8 }
+
+  def self.authenticate_with_credentials (email, password)
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end 
+  end
   
 end
