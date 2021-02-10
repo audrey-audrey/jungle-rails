4c9c2bdcf28d5966167a53e681c83e095e2dc608
audrey-audrey <audreyuu@gmail.com>
2021-02-10 05:01:27 +0000

    Implement password length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: { case_sensitive: false }, presence:true
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :password, length: { minimum: 8 }
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe User, type: :model do
   describe 'Validation: User' do
     it "is valid with matching password and password_confirmation fields" do
       @user = User.new(
-        name: "New User", 
+        first_name: "New",
+        last_name: "User",
         email: "test@test.com", 
         password: "password123", 
         password_confirmation: "password123")
@@ -15,7 +16,8 @@ RSpec.describe User, type: :model do
 
     it "is not valid without matching password and password_confirmation fields" do
       @user = User.new(
-        name: "New User", 
+        first_name: "New",
+        last_name: "User",
         email: "test@test.com", 
         password: "password", 
         password_confirmation: "password123")
@@ -26,14 +28,16 @@ RSpec.describe User, type: :model do
 
     it "is case sensitive and must contain unique email" do
       @user1 = User.new(
-        name: "New User", 
+        first_name: "New",
+        last_name: "User",
         email: "TEST@TEST.com", 
         password: "password123", 
         password_confirmation: "password123")
       @user1.save
 
       @user2 = User.new(
-        name: "Another User", 
+        first_name: "Other",
+        last_name: "User", 
         email: "test@test.COM",
         password: "password456", 
         password_confirmation: "password456")
@@ -42,15 +46,56 @@ RSpec.describe User, type: :model do
       expect(@user2).to_not be_valid
     end
 
-    it "is not valid without name" do
+    it "is not valid without first name" do
       @user = User.new(
-        name: nil, 
+        first_name: nil,
+        last_name: "User",
         email: "test@test.com", 
         password: "password", 
         password_confirmation: "password123")
   
       @user.save
-      expect(@user.error.full_messages).to include("Name can't be blank")
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end
+
+
+    it "is not valid without last name" do
+      @user = User.new(
+        first_name: "New",
+        last_name: nil,
+        email: "test@test.com", 
+        password: "password", 
+        password_confirmation: "password123")
+  
+      @user.save
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
+    end
+
+
+    it "is not valid without email" do
+      @user = User.new(
+        first_name: "New",
+        last_name: "User",
+        email: nil, 
+        password: "password", 
+        password_confirmation: "password123")
+  
+      @user.save
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+  end
+
+  describe 'Password' do
+    it "is not valid if less than 8 characters" do
+      @user = User.new(
+        first_name: "New",
+        last_name: "User",
+        email: "test@test.com", 
+        password: "1234567", 
+        password_confirmation: "1234567")
+  
+      @user.save
+      expect(@user).to_not be_valid
     end
   end
 end
