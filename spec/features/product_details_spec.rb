60a54ace2aae4c9ef4e8ca9f0ef5ba510490e97e
audrey-audrey <audreyuu@gmail.com>
2021-02-11 02:13:55 +0000

    Implement product details test

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-  # SETUP
-  before :each do
+RSpec.feature "ProductDetails", type: :feature, js: true do
+   # SETUP
+   before :each do
     @category = Category.create! name: 'Apparel'
 
     10.times do |n|
@@ -16,14 +16,15 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
-    # ACT
+  scenario "They see product details after clicking on product" do
     visit root_path
 
+    find('article.product img', match: :first).click
+
     # DEBUG
-    save_screenshot("home_page.png")
+    save_screenshot("product_details.png")
 
     # VERIFY
-    expect(page).to have_css 'article.product', count: 10
+    expect(page).to have_css('.products-show')
   end
 end
